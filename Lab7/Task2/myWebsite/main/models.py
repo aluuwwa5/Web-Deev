6cdf6cd5ddd258835c9eb34e658eef4d1158a3d6
Zhanserik <you@example.c git config --global user.name Your>
2023-03-24 11:53:48 +0600

    First Django Project

---
+++ Lab7/Task2/myWebsite/main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Member(models.Model):
+    firstName = models.CharField(max_length=250)
+    lastName = models.CharField(max_length=250)
+    age = models.IntegerField(null=True)
+    address = models.CharField(max_length=200, null=True)
