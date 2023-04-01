01285d026f0c5895678f136224e658a1fc42462d
Zhanserik <zhanserik.kalmukhambet@gmail.com>
2023-04-01 12:45:36 +0600

    Creating views for getting products, product by id, categories, category by id

--- Lab8/shop_back/api/views.py
+++ Lab8/shop_back/api/views.py
@@ -11,3 +11,29 @@ def getProducts(request):
 
     return JsonResponse(list(products), safe=False, json_dumps_params={'indent': 2})
 
+
+def getProductById(request, id):
+    product = Product.objects.filter(pk=id)
+    product = [p.to_json() for p in product]
+
+    return JsonResponse(product, safe=False, json_dumps_params={'indent': 2})
+
+
+def getCategories(request):
+    categories = Category.objects.all().values()
+
+    return JsonResponse(categories, safe=False, json_dumps_params={'indent': 2})
+
+
+def getCategoryById(request, id):
+    category = Category.objects.filter(pk=id)
+    category = [category.to_json() for c in category]
+
+    return JsonResponse(category, safe=False, json_dumps_params={'indent': 2})
+
+
+def getProductsByCategory(request, id):
+    productsByCat = Category.objects.filter(id=id)
+    productsByCat = [p_c.to_json() for p_c in productsByCat]
+
+    return JsonResponse(productsByCat, safe=False, json_dumps_params={'indent': 2})
