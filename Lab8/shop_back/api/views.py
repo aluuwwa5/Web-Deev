f13fe7de65b0ba99b3b89c121c8170b3cf98a0e2
Zhanserik <zhanserik.kalmukhambet@gmail.com>
2023-04-05 00:22:11 +0600

    Filtering and getting data by using ORM in django

--- Lab8/shop_back/api/views.py
+++ Lab8/shop_back/api/views.py
@@ -7,34 +7,39 @@ from .models import Category, Product
 # Create your views here.
 
 def getProducts(request):
-    products = Product.objects.all().values()
+    products = Product.objects.all()
 
-    return JsonResponse(list(products), safe=False, json_dumps_params={'indent': 2})
+    products = [prod.to_json() for prod in products]
+
+    return JsonResponse(products, safe=False, json_dumps_params={'indent': 2})
 
 
 def getProductById(request, id):
-    product = Product.objects.filter(pk=id)
-    product = [p.to_json() for p in product]
+    product = Product.objects.get(id=id)
 
-    return JsonResponse(product, safe=False, json_dumps_params={'indent': 2})
+    return JsonResponse(product.to_json(), safe=False, json_dumps_params={'indent': 2})
 
 
 def getCategories(request):
-    categories = Category.objects.all().values()
+    categories = Category.objects.all()
 
-    return JsonResponse(list(categories), safe=False, json_dumps_params={'indent': 2})
+    categories = [cat.to_json() for cat in categories]
+
+    return JsonResponse(categories, safe=False, json_dumps_params={'indent': 2})
 
 
 def getCategoryById(request, id):
-    category = Category.objects.filter(pk=id)
-    category = [c.to_json() for c in category]
+    category = Category.objects.get(id=id)
 
-    return JsonResponse(category, safe=False, json_dumps_params={'indent': 2})
+    return JsonResponse(category.to_json(), safe=False, json_dumps_params={'indent': 2})
 
 
 def getProductsByCategory(request, id):
-    productsByCat = Category.objects.filter(id=id)
-    productsByCat = [p_c.to_json() for p_c in productsByCat]
+    category = Category.objects.get(id=id)
+
+    productsByCat = Product.objects.filter(category=category)
+
+    productsByCat = [prod.to_json() for prod in productsByCat]
 
     return JsonResponse(productsByCat, safe=False, json_dumps_params={'indent': 2})
 
