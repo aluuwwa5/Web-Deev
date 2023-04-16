0b3ccf2b36865e5455647cce312ee4cf3baccbb4
Zhanserik <zhanserik.kalmukhambet@gmail.com>
2023-04-16 14:25:13 +0600

    make it simple

--- Lab10/hh_back/api/urls.py
+++ Lab10/hh_back/api/urls.py
@@ -4,13 +4,12 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 urlpatterns = [
-    path('companies/', views.CompanyViewSet.as_view({'get': 'list', 'post': 'create'}), name='companies'),
-    path('companies/<int:pk>/', views.CompanyViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='company details'),
     path('companies/<int:pk>/vacancies/', views.CompanyViewSet.as_view({'get': 'list_vacancies'}), name='list of vacancies of company'),
     path('vacancies/top_ten/', views.VacancyViewSet.as_view({'get': 'list_top10'}), name='list of top ten vacancies'),
 ]
 
 r = DefaultRouter()
 r.register(r'vacancies', views.VacancyViewSet, basename='vacancy')
+r.register(r'companies', views.CompanyViewSet, basename='Company')
 
 urlpatterns += r.urls
--- Lab10/hh_back/api/views.py
+++ Lab10/hh_back/api/views.py
@@ -8,38 +8,9 @@ from .serializers import CompanySerializer, VacancySerializer
 from rest_framework import viewsets
 
 
-class CompanyViewSet(viewsets.ViewSet):
-
-    def list(self, request):
-        queryset = Company.objects.all()
-        serializer = CompanySerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = CompanySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Company.objects.all()
-        company = get_object_or_404(queryset, pk=pk)
-        serializer = CompanySerializer(company)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        queryset = Company.objects.all()
-        company = get_object_or_404(queryset, pk=pk)
-        serializer = CompanySerializer(company, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def destroy(self, request, pk=None):
-        queryset = Company.objects.all()
-        company = get_object_or_404(queryset, pk=pk)
-        company.delete()
-        return Response({'deleted': True})
+class CompanyViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
 
     def list_vacancies(self, request, pk=None):
         queryset = Vacancy.objects.all().filter(company=pk).order_by('-salary')[:10]
